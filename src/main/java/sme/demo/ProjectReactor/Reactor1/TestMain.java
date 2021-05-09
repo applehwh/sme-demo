8bcc592dae7fe2c197ac65ef771b47bb28746284
wenhan.hu <1230456kl>
2021-05-09 17:45:52 +0800

    bjdk9

--- src/main/java/sme/demo/ProjectReactor/Reactor1/TestMain.java
+++ src/main/java/sme/demo/ProjectReactor/Reactor1/TestMain.java
@@ -12,16 +12,26 @@ import static reactor.event.selector.Selectors.$;
 
 public class TestMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Environment env = new Environment();
         Reactor reactors = Reactors.reactor()
-                .env(env)
-                .dispatcher(Environment.RING_BUFFER)
+               // .env(env)
+                //Dispatcher：用于管理EventHandler，分发event的容器，也是一个事件处理调度器
+               // .dispatcher(Environment.EVENT_LOOP)
                 .get();
+        //注册事件处理器 EventHandler event->System.out.println(event.getData())
+        //Selector: 事件轮循选择器，selector主要实现了轮循队列中的事件状态，取出当前能够处理的状态
         reactors.on($("channel"),event->System.out.println(event.getData()));
-        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
-                ()->reactors.notify("channel",Event.wrap("test")),
-                0,100, TimeUnit.MILLISECONDS);
+        //Event.wrap("test") Handle 事件
+//        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
+//                ()->reactors.notify("channel",Event.wrap("test")),
+//                0,100, TimeUnit.MILLISECONDS);
+        for (int i = 0; i < 100; i++) {
+            reactors.notify("channel",new Event<String>("test"));
+        }
+     //   Thread.sleep(1000);
+       // reactors.send()
+
 
     }
 
